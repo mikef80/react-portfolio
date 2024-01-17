0349d2de967b8e69587104676c2d90bf3dd73bcf
Mike <mike@mike-francis.org>
2024-01-17 15:14:22 +0000

    working at the moment.  Now for api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { Intro } from "./components/Intro";
+import { Footer } from './components/Footer'
+import {Intro} from './components/Intro'
 
-
-function App() {
+export const App = () => {
   return (
     <>
       <Header />
       <Intro />
-      <Projects />
       <Footer />
     </>
   );
-}
-
-export default App;
+};
