f32540bf770ebe05f3e8cdf18aa0ebef428448e4
Mike <mike@mike-francis.org>
2024-01-21 08:53:25 +0000

    hero in dark mode fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,39 +18,8 @@ export const Header = () => {
     html.classList.toggle("dark");
   };
 
-  const classes = `
-  absolute
-  w-3/4
-  top-1/2
-  left-[12.5%]
-  mix-blend-screen
-  text-center
-  -translate-y-1/2
-  bg-white
-  py-1
-  xs:py-2
-  sm:w-1/2
-  sm:left-1/4
-  `;
-
   return (
     <header className='flex flex-col items-center pb-2'>
-      {/* <div className='relative pb-2'>
-        <img
-          src={Beach}
-          className=' sm:aspect-video sm:aspect-[3.2/1] aspect-[2/1] object-cover'
-          alt=''
-        />
-
-        <div className={classes}>
-          <h1 className='text-3xl sm:text-4xl  md:text-6xl'>Mike Francis</h1>
-
-          <h2 className='pb-2 text-xl  sm:text-2xl md:text-3xl'>
-            Junior Full Stack Developer
-          </h2>
-        </div>
-      </div> */}
-      
       <div className='flex gap-7 pb-2'>
         <a href='https://github.com/mikef80'>
           <img
@@ -63,7 +32,6 @@ export const Header = () => {
           <img src={LinkedInLogo} alt='LinkedIn' />
         </a>
         <button onClick={() => toggleDarkMode()}>
-          {/* <p className="hidden">{darkMode ? 'toggle light mode' : 'toggle dark mode'}</p> */}
           <img
             src={darkMode ? MoonIcon : SunIcon}
             alt='toggle dark mode on and off'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,12 @@ const Hero = () => {
   left-[12.5%]
   mix-blend-screen
   dark:mix-blend-hard-light
-
   text-center
   -translate-y-1/2
   bg-white
   dark:bg-black
+  dark:bg-opacity-80
+  bg-opacity-90
   py-1
   xs:py-2
   sm:w-1/2
@@ -27,11 +28,8 @@ const Hero = () => {
         className='dark:grayscale sm:aspect-video sm:aspect-[3.2/1] aspect-[2/1] object-cover'
         alt='Mike Francis - Junior Full Stack Developer'
       />
-      {/* <img src={Beach} className=" aspect-video aspect-[3.2/1] object-cover" alt='' /> */}
       <div className={classes}>
-        {/* <h1 className='text-6xl '>Mike Francis</h1> */}
         <h1 className='text-3xl sm:text-4xl  md:text-6xl'>Mike Francis</h1>
-        {/* <h2 className='pb-2 text-3xl'> */}
         <h2 className='pb-2 text-xl  sm:text-2xl md:text-3xl'>
           Junior Full Stack Developer
         </h2>
