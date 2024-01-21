cb613ac7a50d201eb20e779212111b32f6232495
Mike <mike@mike-francis.org>
2024-01-21 20:22:04 +0000

    playing with hero dimensions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,11 @@ import Beach from "/beach.jpeg";
 const Hero = () => {
   const classes = `
   absolute
-  w-3/4
+  xs:w-3/4
+  w-5/6
   top-1/2
-  left-[12.5%]
+  left-[8.3%]
+  xs:left-[12.5%]
   mix-blend-screen
   dark:mix-blend-hard-light
   text-center
@@ -17,20 +19,20 @@ const Hero = () => {
   bg-opacity-90
   py-1
   xs:py-2
-  sm:w-1/2
-  sm:left-1/4
+  md:w-1/2
+  md:left-1/4
   `;
 
   return (
     <div className='relative pb-2'>
       <img
         src={Beach}
-        className='dark:grayscale sm:aspect-video sm:aspect-[16/9] aspect-[2/1] object-cover'
+        className='dark:grayscale sm:aspect-[3.2/1] aspect-[2/1] object-cover'
         alt='Mike Francis - Junior Full Stack Developer'
       />
       <div className={classes}>
-        <h1 className='text-3xl sm:text-4xl  md:text-6xl'>Mike Francis</h1>
-        <h2 className='pb-2 text-xl  sm:text-2xl md:text-3xl'>
+        <h1 className='text-4xl sm:text-4xl  md:text-6xl'>Mike Francis</h1>
+        <h2 className='pb-2 text-xl xs:text-2xl md:text-3xl'>
           Junior Full Stack Developer
         </h2>
       </div>
