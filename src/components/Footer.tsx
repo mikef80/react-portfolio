5558d137c5e8763869ebca96921e27eac142d8d6
Mike <mike@mike-francis.org>
2024-01-23 22:57:11 +0000

    started TypeScript conversion

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className='absolute bottom-0 w-full text-xs bg-white h-9 flex justify-center items-center dark:bg-dark-grey'>
+      Made with ☕️
+    </footer>
+  );
+};
