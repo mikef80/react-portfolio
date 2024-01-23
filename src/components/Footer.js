99b11d7d7fd5de4a817d2a0f8d90f0adc3a34cbd
Mike <mike@mike-francis.org>
2024-01-23 23:06:25 +0000

    started TypeScript conversion

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Footer = void 0;
+var react_1 = require("react");
+var Footer = function () {
+    return (<footer className='absolute bottom-0 w-full text-xs bg-white h-9 flex justify-center items-center dark:bg-dark-grey'>
+      Made with ☕️
+    </footer>);
+};
+exports.Footer = Footer;
