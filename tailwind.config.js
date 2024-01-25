bfec0d4f62d66948ae1db94545bd20a3338d4d78
Mike <mike@mike-francis.org>
2024-01-25 16:59:16 +0000

    font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      'sans': ['Roboto']
+    },
     extend: {
       colors: { "dark-grey": "#121219" },
       screens: {
