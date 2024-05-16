9b09a135b6aeded0572221c5e4864a19066e65e2
Mike <mike@mike-francis.org>
2024-05-16 22:37:21 +0100

    added useContext to manage darkMode globally, so I can stop trying to pass state around

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Intro } from "./components/Intro";
 import Projects from "./components/Projects";
-import Hero from "./components/Hero";
+
 
 export const App = () => {
   return (
@@ -10,7 +10,7 @@ export const App = () => {
       <main className='max-w-[540px] mx-auto md:max-w-[768px] lg:max-w-[1000px] flex flex-col min-h-screen'>
         <Header />
         <Intro />
-        <Projects />
+        {/* <Projects /> */}
       </main>
       <Footer />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import "./index.css";
+import { DarkModeProvider } from "./util/DarkModeContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <DarkModeProvider>
     <App />
-  </React.StrictMode>,
-)
+  </DarkModeProvider>
+  // </React.StrictMode>,
+);
