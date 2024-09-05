25b840afc1f87d53021aa435f3a1d46d4553726b
Mike <mike@mike-francis.org>
2024-09-05 21:11:55 +0100

    added repo link and website links where appropriate

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -25,9 +25,14 @@ const Projects = () => {
     },
   ]);
 
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
 
-  /* const [isLoading, setIsLoading] = useState(true);
+  const darkModeStyle = {
+    filter: "drop-shadow(0px 1000px 0 white)",
+    transform: "translateY(-1000px)",
+  };
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
@@ -35,7 +40,7 @@ const Projects = () => {
       .then((arr) => setRepos(arr))
       .then(() => setIsLoading(false));
   }, []);
- */
+
   return (
     <div className='px-2 pb-9 flex flex-col grow'>
       <h3 className='text-2xl pb-2'>Projects</h3>
@@ -43,21 +48,35 @@ const Projects = () => {
         <Loading />
       ) : (
         <ul className='flex flex-col md:grid md:grid-cols-2 gap-3 md:gap-y-5'>
-          {repos.map(({ id, name, description, url }) => {
+          {repos.map(({ id, name, description, url, homepage }) => {
             return (
               <li
                 key={id}
                 className={`border-b-[1px] border-gray-300 p-4 last-of-type:border-0 md:last-of-type:border-b-[1px] `}>
-                <a href={url}>
-                  <div className='flex justify-between'>
-                    <h4 className='text-xl pb-1'>{name}</h4>
-                    <div className='flex justify-between'>
-                      <img src={darkMode ? CodeDark : Code} alt='' style={{color:"white"}} />
-                      <img src={darkMode ? WebDark : Web} alt='' />
+                {/* <a href={url}> */}
+                  <div className='flex justify-between mb-1'>
+                    <h4 className='text-xl'>{name}</h4>
+                    <div className='flex justify-between overflow-hidden gap-2'>
+                      <a href={url}>
+                        <img
+                          src={Code}
+                          alt=''
+                          style={darkMode ? darkModeStyle : {}}
+                          className='h-7'
+                        />
+                      </a>
+                      {homepage && <a href={homepage}>
+                        <img
+                          src={Web}
+                          alt=''
+                          style={darkMode ? darkModeStyle : {}}
+                          className='h-7'
+                        />
+                      </a>}
                     </div>
                   </div>
                   <p className='text-gray-500 dark:text-gray-400'>{description}</p>
-                </a>
+                {/* </a> */}
               </li>
             );
           })}
